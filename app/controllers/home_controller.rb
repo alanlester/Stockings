689e0e81ce8a8a8ea14c8c0e73a4294e1aacd6bc
Alan Lester <aclgmc03@gmail.com>
2022-10-02 18:27:43 -0700

    100222

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,14 @@
 class HomeController < ApplicationController
   def index
+       @api = StockQuote::Stock.new(api_key: 'sk_03de06d48e204c858c4b8980cd9484a0')
+
+       @test = "test"
+       if params[:ticker]
+          @stock = StockQuote::Stock.quote(params[:ticker])
+       end
   end
 
 def about
 end
+
 end
